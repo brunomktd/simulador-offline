bfba32ecaaf80796da645ba26db565b8bcc4e45e
Bruno Oliveira <bruno.mktd@gmail.com>
2020-06-16 09:12:56 -0300

    Ajustes de bugs

---
+++ src/main/java/br/com/embracon/controller/SimulacaoController.java
@@ -0,0 +1,25 @@
+package br.com.embracon.controller;
+
+import br.com.embracon.controller.response.PlanosResponse;
+import br.com.embracon.service.SimulacaoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/simulacoes")
+public class SimulacaoController {
+
+    @Autowired
+    private SimulacaoService simulacaoService;
+
+    @GetMapping("/planos/{idBem}")
+    public List<PlanosResponse> buscaPlanosPorCredito(
+            @PathVariable Integer idBem,
+            @RequestParam(required = false) Integer creditoSolicitado){
+
+         return simulacaoService.obterPlanos(idBem, creditoSolicitado);
+    }
+
+}
