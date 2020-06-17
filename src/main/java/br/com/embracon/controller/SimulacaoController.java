84e9e82861b7dbddfa69d9ca9225618c881ccde2
Bruno Oliveira <bruno.mktd@gmail.com>
2020-06-17 14:08:00 -0300

    Validação de campos

--- src/main/java/br/com/embracon/controller/SimulacaoController.java
+++ src/main/java/br/com/embracon/controller/SimulacaoController.java
@@ -1,6 +1,7 @@
 package br.com.embracon.controller;
 
-import br.com.embracon.controller.response.PlanosResponse;
+import br.com.embracon.controller.response.PlanosPorBensResponse;
+import br.com.embracon.controller.response.PlanosPorCreditoResponse;
 import br.com.embracon.service.SimulacaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -15,9 +16,16 @@ public class SimulacaoController {
     private SimulacaoService simulacaoService;
 
     @GetMapping("/planos/{idBem}")
-    public List<PlanosResponse> buscarPlanos(
+    public List<PlanosPorBensResponse> buscarPlanos(
+            @PathVariable Integer idBem){
+
+        return simulacaoService.obterPlanosPorTipoDeBem(idBem);
+    }
+
+    @GetMapping("/planos/{idBem}/creditos")
+    public List<PlanosPorCreditoResponse> buscarPlanos(
             @PathVariable Integer idBem,
-            @RequestParam(required = false) Integer creditoSolicitado){
+            @RequestParam Integer creditoSolicitado){
 
          return simulacaoService.obterPlanos(idBem, creditoSolicitado);
     }
